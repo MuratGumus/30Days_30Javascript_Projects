9c5b9a45bed29281c3258f1f8039d616b13c6f7d
gumusmuratgm@gmail.com <gumusmuratgm@gmail.com>
2023-06-20 23:23:10 +0300

    08 added

---
+++ 07_Weather_App/app.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+const input = document.querySelector('form #input-alani');
+
+form.addEventListener("click", (e) => {
+    e.preventDefault()
+    e.currentTarget.reset()
+})
+
+const getData = async;
